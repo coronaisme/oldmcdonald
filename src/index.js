67d950a3704e84afbb253de042e272aebe591208
coronaisme <coronaisme@gmail.com>
2020-10-14 13:56:11 -0700

    clean up

--- src/index.js
+++ src/index.js
@@ -15,84 +15,74 @@
 
 // import "./styles.css";
 
-
 const animal1 = {
   id: 1,
-  species : "cow",
-  avatar : "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwiseacre-gardens.com%2Fcows%2Fcow-redhead-face.jpg&f=1&nofb=1",
+  species: "cow",
+  avatar:
+    "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwiseacre-gardens.com%2Fcows%2Fcow-redhead-face.jpg&f=1&nofb=1",
   name: "Bessie",
   gender: "female",
-  owner: "David"
-}
+  owner: "David",
+};
 
 const animal2 = {
   id: 2,
-  species : "pig",
-  avatar : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs3-eu-west-1.amazonaws.com%2Ffolly-farm.co.uk%2Fuploads%2F2018%2F04%2Fpig-810x1024.jpg&f=1&nofb=1",
+  species: "pig",
+  avatar:
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs3-eu-west-1.amazonaws.com%2Ffolly-farm.co.uk%2Fuploads%2F2018%2F04%2Fpig-810x1024.jpg&f=1&nofb=1",
   name: "Pig",
   gender: "female",
-  owner: "David"
-}
+  owner: "David",
+};
 const animal3 = {
   id: 3,
-  species : "chicken",
-  avatar : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.justataste.com%2Fwp-content%2Fuploads%2F2014%2F09%2Froast-chicken-garlic-lemon-recipe.jpg&f=1&nofb=1",
+  species: "chicken",
+  avatar:
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.justataste.com%2Fwp-content%2Fuploads%2F2014%2F09%2Froast-chicken-garlic-lemon-recipe.jpg&f=1&nofb=1",
   name: "Chicken",
   gender: "female",
-  owner: "David"
-}
+  owner: "David",
+};
 
-
-const animalArray = [animal1, animal2, animal3]
-
+const animalArray = [animal1, animal2, animal3];
 
 const penArray = [
-{
-  title:"cow",
-  animals:[]
-},
-{
-  title:"chicken",
-  animals:[]
-},
-{
-  title:"pig",
-  animals:[]
-},
-{
-  title:"sheep",
-  animals:[]
-}
-]
-
+  {
+    title: "cow",
+    animals: [],
+  },
+  {
+    title: "chicken",
+    animals: [],
+  },
+  {
+    title: "pig",
+    animals: [],
+  },
+  {
+    title: "sheep",
+    animals: [],
+  },
+];
 
 const addToPen = () => {
-    animalArray.forEach(animal => {
-       penArray.find(pen => {
-         return pen.title === animal.species  
-        }).animals.push(animal)
-    })
-}
+  animalArray.forEach((animal) => {
+    penArray
+      .find((pen) => {
+        return pen.title === animal.species;
+      })
+      .animals.push(animal);
+  });
+};
 
-addToPen()
-
-
+addToPen();
 
 const handleSubmit = (e) => {
-  e.preventDefault()
-  console.log(e.target)
-}
-
-
-
-
-
-
+  e.preventDefault();
+  console.log(e.target);
+};
 
 const renderAnimalForm = () => {
-
-
-
   return `
   <h5 class="creation">Create an Animal</h5>
 
@@ -116,14 +106,12 @@ const renderAnimalForm = () => {
   <input type="submit" id="submit" />
   
   
-  </form>`
-}
-
-
+  </form>`;
+};
 
 const renderAnimal = (animal) => {
   return `<div class="animal" id=${animal.id}>
-            <h2>${ animal.name }</h2> 
+            <h2>${animal.name}</h2> 
             
             <h3>${animal.gender}</h3></br>
 
@@ -131,80 +119,69 @@ const renderAnimal = (animal) => {
 
             <p> Owner: ${animal.owner}</p>
 
-          </div>`
-}
+          </div>`;
+};
 
 const renderPen = (penArray) => {
+  let str = "";
+  let currentPenStr = "";
 
-  let str = '';
-  let currentPenStr = '';
+  penArray.map((pen) => {
+    pen.animals.map((animal) => {
+      let penContent = "";
 
-  penArray.map(pen => {   
-    
-    pen.animals.map(animal => {
-    let penContent = ''
-      
-      
-      penContent += renderAnimal(animal)
-      currentPenStr = `<div class="penContent"> ${penContent} </div>`
-      
-    })
-    str += currentPenStr
-  })
+      penContent += renderAnimal(animal);
+      currentPenStr = `<div class="penContent"> ${penContent} </div>`;
+    });
+    str += currentPenStr;
+  });
 
   return `<div class="animalPen">
             
             ${str}
   
-         </div>`
-}
-
-
-
-
+         </div>`;
+};
 
 const createAnimal = (species, avatar, name, gender, owner) => {
+  let nextId = animalArray[animalArray.length - 1].id + 1;
 
-    
-    let nextId = animalArray[animalArray.length - 1].id + 1
+  return {
+    id: nextId,
+    species: species,
+    avatar: avatar,
+    name: name,
+    gender: gender,
+    owner: owner,
+  };
+};
 
-    return {
-      id:nextId,
-      species:species,
-      avatar:avatar,
-      name:name,
-      gender:gender,
-      owner:owner
-      
-    }
-}
+window.addEventListener("DOMContentLoaded", (event) => {
+  let barnyard = document.getElementById("barnyard");
+  let form = document.getElementById("form");
 
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-  let barnyard = document.getElementById("barnyard")
-  let form = document.getElementById("form")
-
-
-  barnyard.innerHTML += renderPen(penArray)
-  form.innerHTML += renderAnimalForm()
-
+  barnyard.innerHTML += renderPen(penArray);
+  form.innerHTML += renderAnimalForm();
 
   form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    let speciesVal = document.getElementById("speciesId").value
-    let avatarVal = document.getElementById("avatarId").value
-    let nameVal = document.getElementById("nameId").value
-    let genderVal = document.getElementById("genderId").value
-    let ownerVal = document.getElementById("ownerId").value
+    e.preventDefault();
+    let speciesVal = document.getElementById("speciesId").value;
+    let avatarVal = document.getElementById("avatarId").value;
+    let nameVal = document.getElementById("nameId").value;
+    let genderVal = document.getElementById("genderId").value;
+    let ownerVal = document.getElementById("ownerId").value;
 
-    let newAnimal = createAnimal(speciesVal, avatarVal, nameVal, genderVal, ownerVal)
+    let newAnimal = createAnimal(
+      speciesVal,
+      avatarVal,
+      nameVal,
+      genderVal,
+      ownerVal
+    );
 
-    animalArray.push(newAnimal)
-    barnyard.innerHTML = ""
-    barnyard.innerHTML += renderPen(animalArray)
-    console.log(animalArray, "array of animals")
-
-  })
-
+    animalArray.push(newAnimal);
+    barnyard.innerHTML = "";
+    barnyard.innerHTML += renderPen(animalArray);
+    console.log(animalArray, "array of animals");
+  });
 });
